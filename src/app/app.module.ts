e447bb7739bf510ff180031f0966edb2bf2ca8ec
Roopak K Prajapat <rkprajapat@Roopaks-MacBook-Pro.local>
2018-05-18 21:02:13 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialModule} from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavResponsiveComponent } from './sidenav-responsive/sidenav-responsive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavResponsiveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    NgbModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, SidenavResponsiveComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
